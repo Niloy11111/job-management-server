accad4ed83d9222b2619e4b797c164eb44fe22e2
Niloy11111 <Ektiaruddinniloy859@gmail.com>
2023-11-06 00:17:04 +0600

    get allJobs from allJobsCollection and each single jobs by _id using get api method

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express') ;
 const cors = require('cors') ;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config() ;
 const app = express() ;
 const port = process.env.PORT || 5000 ;
@@ -26,7 +26,48 @@ async function run() {
   try {
     // await client.connect();
 
+    const allJobsCollection = client.db("AllJobsDB").collection("jobsCollection");
 
+    app.get('/allJobs', async (req, res) => { 
+      const cursor = allJobsCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+  })
+
+  app.get('/jobs/Remote', async(req, res) => {
+    const cursor = allJobsCollection.find({'jobCategory' : 'Remote'})
+    const result = await cursor.toArray() ;
+    res.send(result)
+  })
+
+  app.get('/jobs/OnSite', async(req, res) => {
+    const cursor = allJobsCollection.find({'jobCategory' : 'On Site'})
+    const result = await cursor.toArray() ;
+    res.send(result)
+  })
+  app.get('/jobs/Part-Time', async(req, res) => {
+    const cursor = allJobsCollection.find({'jobCategory' : 'Part-Time'})
+    const result = await cursor.toArray() ;
+    res.send(result)
+  })
+  app.get('/jobs/Hybrid', async(req, res) => {
+    const cursor = allJobsCollection.find({'jobCategory' : 'Hybrid'})
+    const result = await cursor.toArray() ;
+    res.send(result)
+  })
+
+  app.get('/allJobs/:id', async (req, res) => {
+    const id = req.params.id;
+    const query = { _id: new ObjectId(id) }
+    const result = await allJobsCollection.findOne(query);
+    res.send(result);
+})
+
+    app.post('/addJob', async(req, res) => {
+      const newJob = req.body ;
+      const result = await allJobsCollection.insertOne(newJob) ;
+      res.send(result)
+    })
 
 
     // Send a ping to confirm a successful connection
